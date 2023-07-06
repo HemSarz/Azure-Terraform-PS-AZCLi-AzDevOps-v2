5eaaad6dda7d22d43dc0ed9ca9ecab2e71d0c26d
Hemen Sarzali <sarzali7_88@hotmail.com>
2023-07-06 12:58:22 +0200

    UpdateTFsv1

--- preconfigres.ps1
+++ preconfigres.ps1
@@ -9,12 +9,12 @@ $backend_location = "norwayeast"
 $backendAzureRmKey = "terraform.tfstate"
 
 # Key Vault variables
-$backend_kv = "bcknd-kvsc-tfaz"
+$backend_kv = "bcknd-tfaz-sec-kv"
 
 # Key Vault Secret Names
-$backend_AZDOSrvConnName_kv_sc = "AZDONamec"
+$backend_AZDOSrvConnName_kv_sc = "AZDOName"
 $backend_RGName_kv_sc = "RGName"
-$backend_STGPass_Name_kv_sc = "STGNamec"
+$backend_STGPass_Name_kv_sc = "STGName"
 $backend_ContName_kv_sc = "ContName"
 $backendAzureRmKey_kv_sc = "TFStatefileName"
 $backend_SUBid_Name_kv_sc = "SUBidName"
@@ -25,7 +25,6 @@ $backend_SPNPass_Name_kv_sc = "SPNPass"
 # Set the Azure DevOps organization and project details
 $backend_org = "https://dev.azure.com/tfazlab"
 $backend_project = "tfazlab"
-$backend_pat = "ftyhdbvr2xjqnp5345hl3vxaqbkb7aueobbsfhs7fbct66ufw2fq"
 
 # Set the variable group details
 $backend_VBGroup = "hawaVB"
@@ -42,15 +41,17 @@ $backend_SPNPass = $(az ad sp create-for-rbac --name $backend_spn --role $backen
 
 Start-Sleep -Seconds 2
 
+# Set the SPN password as an environment variable: used by the Azdo Service Connection
+$env:AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY=$backend_SPNPass
+
+Start-Sleep -Seconds 2
+
 Write-Host "Creating resource group..." -ForegroundColor Yellow
 az group create --name $backend_rg --location $backend_location
 
 Write-Host "Creating storage account..." -ForegroundColor Yellow
 az storage account create --resource-group $backend_rg --name $backend_stg --sku $backend_stg_sku --encryption-services blob
 
-Start-Sleep -Seconds 2
-
-Write-Host "Retrieving storage account access key..." -ForegroundColor Yellow
 $backend_STGPass = $(az storage account keys list --resource-group $backend_rg --account-name $backend_stg --query "[0].value" -o tsv)
 
 Start-Sleep -Seconds 2
@@ -76,79 +77,74 @@ $backend_SUBid = $(az account show --query 'id' -o tsv)
 $backend_SUBName = $(az account show --query 'name' -o tsv)
 $backend_TNTid = $(az account show --query 'tenantId' -o tsv)
 
+Write-Host "Setting Azure DevOps Service Connection Name secret..." -ForegroundColor Yellow
+az keyvault secret set --vault-name $backend_kv --name $backend_AZDOSrvConnName_kv_sc --value $backend_AZDOSrvConnName
 Start-Sleep -Seconds 2
 
-Write-Host -ForegroundColor Yellow "Setting Azure DevOps Service Connection Name secret..."
-az keyvault secret set --vault-name $backend_kv --name $backend_AZDOSrvConnName_kv_sc --value $backend_AZDOSrvConnName
-
-Write-Host -ForegroundColor Yellow "Setting Resource Group Name secret..."
+Write-Host "Setting Resource Group Name secret..." -ForegroundColor Yellow
 az keyvault secret set --vault-name $backend_kv --name $backend_RGName_kv_sc --value $backend_rg
+Start-Sleep -Seconds 2
 
-Write-Host -ForegroundColor Yellow "Setting Storage Account Password secret..."
+Write-Host "Setting Storage Account Password secret..." -ForegroundColor Yellow
 az keyvault secret set --vault-name $backend_kv --name $backend_STGPass_Name_kv_sc --value $backend_stg
+Start-Sleep -Seconds 2
 
-Write-Host -ForegroundColor Yellow "Setting Container Name secret..."
+Write-Host "Setting Container Name secret..." -ForegroundColor Yellow
 az keyvault secret set --vault-name $backend_kv --name $backend_ContName_kv_sc --value $backend_cont
+Start-Sleep -Seconds 2
 
-Write-Host -ForegroundColor Yellow "Setting Azure Resource Manager Key secret..."
+Write-Host "Setting Azure Resource Manager Key secret..." -ForegroundColor Yellow
 az keyvault secret set --vault-name $backend_kv --name $backendAzureRmKey_kv_sc --value $backendAzureRmKey
+Start-Sleep -Seconds 2
 
-Write-Host -ForegroundColor Yellow "Setting Subscription ID secret..."
+Write-Host "Setting Subscription ID secret..." -ForegroundColor Yellow
 az keyvault secret set --vault-name $backend_kv --name $backend_SUBid_Name_kv_sc --value $backend_SUBid
+Start-Sleep -Seconds 2
 
-Write-Host -ForegroundColor Yellow "Setting Tenant ID secret..."
+Write-Host "Setting Tenant ID secret..." -ForegroundColor Yellow
 az keyvault secret set --vault-name $backend_kv --name $backend_TNTid_Name_kv_sc --value $backend_TNTid
+Start-Sleep -Seconds 2
 
-Write-Host -ForegroundColor Yellow "Adding the Storage Account Access Key to Key Vault..."
+Write-Host "Adding the Storage Account Access Key to Key Vault..." -ForegroundColor Yellow
 az keyvault secret set --vault-name $backend_kv --name $backend_STGPass_Name_kv_sc --value $backend_STGPass
+Start-Sleep -Seconds 2
 
-Write-Host -ForegroundColor Yellow "Adding the Service Principal Password to Key Vault..."
+Write-Host "Setting SPN secret..." -ForegroundColor Yellow
 az keyvault secret set --vault-name $backend_kv --name $backend_SPNPass_Name_kv_sc --value $backend_SPNPass
+Start-Sleep -Seconds 2
 
 ################################################################################
 
-# Set Service Principal Secret as an Environment Variable for creating Azure DevOps Service Connection
-$env:AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY = $backend_SPNPass
-
-# Set PAT as an environment variable for DevOps Login
-$env:AZURE_DEVOPS_EXT_PAT = $backend_pat
+# Set Default DevOps Organisation and Project
+az devops configure --defaults organization=$backend_org
+az devops configure --defaults project=$backend_project
 
 Write-Host "Creating Azure DevOps service endpoint..." -ForegroundColor Yellow
-
-# Set Default DevOps Organisation and Project
-az devops configure --defaults organization=$backend_org --verbose
-az devops configure --defaults project=$backend_project --verbose
-
 # Create DevOps Service Connection
 az devops service-endpoint azurerm create --azure-rm-service-principal-id $backend_SPNAppID --azure-rm-subscription-id $backend_SUBid --azure-rm-subscription-name $backend_SUBName --azure-rm-tenant-id $backend_TNTid --name $backend_AZDOSrvConnName --org $backend_org --project $backend_project
 
 Start-Sleep -Seconds 5
 
 Write-Host "Creating the variable group..." -ForegroundColor Yellow
+az pipelines variable-group create --organization $backend_org --project $backend_project --name $backend_VBGroup --description $description --variables foo=bar --authorize true
 
-az pipelines variable-group create --organization $backend_org --project $backend_project --name $backend_VBGroup --description $description --variables foo=bar
 $backend_VBGroupID = $(az pipelines variable-group list --organization $backend_org --project $backend_project --query "[?name=='$backend_VBGroup'].id" -o tsv)
 
-# Update the variable group to link it to Azure Key Vault
-az pipelines variable-group update --id $backend_VBGroupID --org $backend_org --project $backend_project --reference-name $backend_kv --reference-type "azureKeyVault" --authorize true
+# Update the variable group to link it to Authorize
+az pipelines variable-group update --id $backend_VBGroupID --org $backend_org --project $backend_project --authorize true
 
 Start-Sleep -Seconds 5
 
-Write-Host "Linking the Key Vault reference to the variable group..." -ForegroundColor Yellow
-
-az keyvault secret list --vault-name $backend_kv --query "[].{name:name, value:attributes.secretValue}" -o json | ConvertFrom-Json | ForEach-Object {
-    $name = $_.name
-    $value = $_.value
-    az pipelines variable-group variable create --group-id $backend_VBGroupID --name $name --value $value --secret true
-}
+Write-Host "Linking the Key Vault secrets to the variable group..." -ForegroundColor Yellow
 
 Write-Host "Creating pipeline for tfazlab project..." -ForegroundColor Yellow
 az pipelines create --name 'TFazInfraPipe' --description 'Pipeline for tfazlab project' --detect false --repository $backend_RepoName --branch main --yml-path tfazbuild.yml --repository-type tfsgit --skip-first-run true
 
+Start-Sleep -Seconds 10
+
 Write-Host "Allowing AZDO ACCESS..." -ForegroundColor Yellow
 # Grant Access to all Pipelines to the Newly Created DevOps Service Connection
-
 $backend_EndPid = az devops service-endpoint list --query "[?name=='$backend_AZDOSrvConnName'].id" -o tsv
-az devops service-endpoint update --detect false --id $backend_EndPid --enable-for-all true --verbose
+az devops service-endpoint update --detect false --id $backend_EndPid --enable-for-all true
 
 Write-Host "Done!" -ForegroundColor Green
